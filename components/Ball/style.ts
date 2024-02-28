274f5e88471536d2edc10a95d1f98980f70d00e8
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-02-28 19:51:23 -0300

    feat: style

---
+++ components/Ball/style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        position: "absolute",
+        height: 30,
+        width: 30,
+        borderRadius: 15,
+        backgroundColor: '#FFFFFF',
+    }
+});
+
+export default styles
